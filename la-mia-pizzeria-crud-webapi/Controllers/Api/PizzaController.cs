6830d5f2fc9be98d661787ede780c5d6be2de721
Levva01 <vellanifederico13@gmail.com>
2022-10-17 00:17:41 +0200

    api created

---
+++ la-mia-pizzeria-crud-webapi/Controllers/Api/PizzaController.cs
@@ -0,0 +1,26 @@
+﻿using la_mia_pizzeria_crud_mvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.X509Certificates;
+
+namespace la_mia_pizzeria_crud_mvc.Controllers.Api
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+
+    public class PizzaController : Controller
+    {
+        ApplicationDbContext context = new ApplicationDbContext();
+
+        public PizzaController()
+        {
+
+        }
+
+        public IActionResult Get()
+        {
+            List<Pizza> pizze = context.Pizze.ToList();
+
+            return Ok(pizze);
+        }
+    }
+}
