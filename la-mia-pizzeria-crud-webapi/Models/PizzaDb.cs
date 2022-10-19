d76069a228788c410b7362112874248894999e48
Levva01 <vellanifederico13@gmail.com>
2022-10-19 16:43:53 +0200

    PizzaRepository model created

---
+++ la-mia-pizzeria-crud-webapi/Models/PizzaDb.cs
@@ -0,0 +1,6 @@
+﻿namespace la_mia_pizzeria_crud_webapi.Models
+{
+    public class PizzaDb
+    {
+    }
+}
---
+++ la-mia-pizzeria-crud-webapi/Models/PizzaRepository.cs
@@ -0,0 +1,14 @@
+﻿using la_mia_pizzeria_crud_mvc.Models;
+
+namespace la_mia_pizzeria_crud_webapi.Models
+{
+    public class PizzaRepository
+    {
+        public List<Pizza> GetList();
+        public List<Pizza> GetListByFilter(string search);
+        public Pizza GetById(int id);
+        public void Create(Pizza pizza);
+        public void Update(Pizza pizza);
+        public void Delete(Pizza pizza);
+    }
+}
